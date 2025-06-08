e2ad8416ed086f1d566ffa2d31d31ec5aececf34
bozhenakv <bkvartuk@gmail.com>
2025-06-08 04:45:40 +0300

    Pridane funkcie na vlozenie noveho uzla napravo v cyklickom obojsmernom zozname, vytvorenie zoznamu s nahodnymi hodnotami, vypis zoznamu a uvolnenie pamate.

--- 11.5.18.kvbozhena.c
+++ 11.5.18.kvbozhena.c
@@ -21,3 +21,68 @@ CTWL *ctwl_create_empty(void){
 	new_list->cur = NULL;
 	return new_list;
 }
+
+TWN *ctwl_insert_right(CTWL* list, float val){
+	if (list == NULL){
+		return NULL;
+	}
+	TWN *new_node = (TWN*)malloc(sizeof(TWN));
+	if (new_node == NULL){
+		return NULL;
+	}
+	new_node->data = val;
+	if(list->cur == NULL){
+		list->cur = new_node;
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		return new_node;
+	}
+	TWN *cur = list->cur;
+	TWN *right = cur->next;
+	
+	new_node->prev = cur;
+    new_node->next = right;
+    cur->next = new_node;
+    right->prev = new_node;
+	
+	return new_node;
+}
+
+
+CTWL *ctwl_create_random(unsigned int size){
+	CTWL *list = ctwl_create_empty();
+	for (unsigned int i = 0; i < size; i++){
+		float val = (float)(rand()%100);
+		ctwl_insert_right(list, val);
+    }
+    return list;    
+}
+
+void ctwl_print(CTWL *list){
+    if (list == NULL || list->cur == NULL){
+        printf("Zoznam je prazdny\n");
+        return;
+    }	
+    TWN *temp_cur = list->cur;
+    do{
+    	printf("%.1f ", temp_cur->data);
+    	temp_cur = temp_cur->next;
+	}while (temp_cur != list->cur);
+}
+
+void ctwl_destroy(CTWL* list){
+	if (list == NULL || list->cur == NULL){
+		return;
+	}
+	TWN *cur = list->cur;
+	TWN *temp_cur = cur->next;
+	
+	while (temp_cur != cur){
+	    TWN *to_free = temp_cur;
+		temp_cur = temp_cur->next;	
+		free(to_free);
+	}
+	
+	free(cur);
+	free(list);
+}
